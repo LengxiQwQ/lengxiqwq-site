2400d1cc473f46a05f84551848c581ecec650a26
LengxiQwQ <lengxiowo@gmail.com>
2026-09-26 03:50:38 +0800

    fix: 彻底修复 iOS Safari 深色模式下链接变蓝与 Sticky Hover 问题

--- src/components/controls/Search.svelte
+++ src/components/controls/Search.svelte
@@ -235,7 +235,7 @@ top-20 left-4 md:left-[unset] right-4 shadow-2xl rounded-2xl p-2"
                on:click={(e) => handleResultClick(e, item.url)}
                class="transition first-of-type:mt-2 lg:first-of-type:mt-0 group block
            rounded-xl text-lg px-3 py-2 hover:bg-(--btn-plain-bg-hover) active:bg-(--btn-plain-bg-active)">
-                <div class="transition text-90 inline-flex font-bold group-hover:text-(--primary)">
+                <div class="transition text-90 inline-flex font-bold group-md:hover:text-(--primary)">
                     {@html item.meta.title}
                     <Icon icon="fa7-solid:chevron-right"
                           class="transition text-[0.75rem] translate-x-1 my-auto text-(--primary)"></Icon>
